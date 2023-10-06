5f6476bb47bce9c242af4718521433d2dd85c3c3
amankashyap004 <amankashyap004@gmail.com>
2023-10-06 21:31:11 +0530

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main className="text-white ">
       <div className="flex justify-center items-center p-4">
-        <h1>My portfolio</h1>
+        <h1>Aman's portfolio </h1>
       </div>
     </main>
   );
